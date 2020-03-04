741dc15b3e79751bc4cdf5e0f9492393e75fe288
ghivalla <ghivalla@gmail.com>
2020-03-05 00:46:43 +0100

    implemented the mailing from the server

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const express = require('express')
+const app = express()
+app.use(express.json());
+
+app.post('/send-email', function (req, res) {
+    const { email, name, message } = req.body
+    const msg = {
+        to: 'ghivalla.soumar@gmail.com',
+        from: email,
+        subject: `Website : New message from ${name}`,
+        text: message,
+        html: `<p>${message}</p>`,
+    };
+    (async () => {
+        try {
+          await sgMail.send(msg);
+          res.status(200).json('message sent successfully')
+        } catch (err) {
+          console.error(err.toString());
+          res.status(400).json('something went wrong...')
+        }
+    })();
+})
+
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!')
+})
